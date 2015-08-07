a2911981db043c008d664304332d233115ada8d2
pouc <loic.formont@gmail.com>
2015-08-07 16:07:18 +0200

    Commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,34 @@
-/**
- * Created by lft on 07/08/2015.
- */
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jsdoc2md: {
+            oneOutputFile: {
+                src: 'index.js',
+                dest: 'README.md'
+            }
+        },
+        gitcommit: {
+            task: {
+                options: {
+                    allowEmpty: true,
+                    message: 'Commit'
+                },
+                files: {
+                    src: ['*.js']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-jsdoc-to-markdown");
+    grunt.loadNpmTasks('grunt-git');
+
+    grunt.registerTask('default', [])
+    grunt.registerTask('generate doc', 'jsdoc2md')
+    grunt.registerTask('commit github', 'gitcommit')
+
+};
+
+
